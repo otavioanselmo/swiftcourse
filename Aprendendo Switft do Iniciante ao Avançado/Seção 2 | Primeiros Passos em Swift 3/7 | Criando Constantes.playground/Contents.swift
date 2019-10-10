cda8021bb75b60a3c4147c9a86b105991793ca4f
Otávio Anselmo <otavio.aanselmo@gmail.com>
2019-10-10 12:49:58 -0300

    Att

---
+++ Aprendendo Switft do Iniciante ao Avançado/Seção 2 | Primeiros Passos em Swift 3/7 | Criando Constantes.playground/Contents.swift
@@ -0,0 +1,11 @@
+// Exemplificando a possibilidade de mudança em uma variável
+var nome = "Lucas"
+nome = "João"
+nome
+
+// Em Swift, as constantes são declaradas com o comando LET
+let cachorro = "Sparky"
+
+// Exercicio da Aula
+var meuNome = "Otávio"
+let minhaIdade = 21
