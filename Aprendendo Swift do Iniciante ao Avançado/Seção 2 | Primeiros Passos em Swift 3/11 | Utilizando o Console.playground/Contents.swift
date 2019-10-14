4e2e5fd8e5c1b63e7edf4db3d39e462aa95bce56
Otávio Anselmo <otavio.aanselmo@gmail.com>
2019-10-14 10:50:42 -0300

    aula11

---
+++ Aprendendo Swift do Iniciante ao Avançado/Seção 2 | Primeiros Passos em Swift 3/11 | Utilizando o Console.playground/Contents.swift
@@ -0,0 +1,21 @@
+var primeiroNome = "Lucas"
+var sobrenome = "Alencar"
+
+// Método print()
+
+// Concatenação
+print(primeiroNome + " " + sobrenome)
+print("")
+
+// Interpolação print("\(VARIAVÉL AQUI) \(VARIÁVEL AQUI")
+print("Meu primeiro nome é \(primeiroNome) e meu último nome é \(sobrenome)")
+print("")
+
+
+
+// Exemplo
+
+var numero1 = 10
+var numero2 = 20
+
+print("O meu primeiro número é: \(numero1). O meu segundo número é: \(numero2). A soma dos dois número é: \(numero1 + numero2).")
