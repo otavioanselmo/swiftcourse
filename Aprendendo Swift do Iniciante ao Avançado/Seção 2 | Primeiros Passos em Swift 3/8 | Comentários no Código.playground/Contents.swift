6460af52c6ab802dd09dd12bae984b3ca206eb00
Otávio Anselmo <otavio.aanselmo@gmail.com>
2019-10-18 15:16:48 -0300

    classes_15and16

--- Aprendendo Swift do Iniciante ao Avançado/Seção 2 | Primeiros Passos em Swift 3/8 | Comentários no Código.playground/Contents.swift
+++ Aprendendo Swift do Iniciante ao Avançado/Seção 2 | Primeiros Passos em Swift 3/8 | Comentários no Código.playground/Contents.swift
@@ -1,7 +1,7 @@
 // Exemplo de comentário em uma linha
 
 /*
- Aqui voc pode escrever várias linhas de comentário
+ Aqui você pode escrever várias linhas de comentário
  1º comentário
  
  2º comentário
