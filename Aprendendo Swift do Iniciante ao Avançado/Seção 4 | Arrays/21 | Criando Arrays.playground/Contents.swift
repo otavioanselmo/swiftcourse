c2025ae28812673b4752c86fddd81fd68a199b08
Otávio Anselmo <otavio.aanselmo@gmail.com>
2019-10-20 21:49:52 -0300

    session4_classes20to23
    Starting nem Session with 4 classes

---
+++ Aprendendo Swift do Iniciante ao Avançado/Seção 4 | Arrays/21 | Criando Arrays.playground/Contents.swift
@@ -0,0 +1,11 @@
+// Criando uma array com conteúdo INT de forma IMPLÍCITA
+var datasDeCopaDoMundo = [1958, 1962, 1970, 1994, 2002]
+
+// Criando uma array com conteúdo INT de forma EXPLÍTITA
+var datasDeCopaDoMundo2 : [Int] = [1958, 1962, 1970, 1994, 2002]
+
+// Criando uma array com conteúdo STRING de forma IMPLÍCITA
+var filmesQueGosto = ["Tropa de Elite", "Toy Story 1, 2 e 3", "Operação Big Hero"]
+
+// Criando uma array com conteúdo STRING de forma EXPLÍCITA
+var filmeQueGosto2 : [String] = ["Tropa de Elite", "Toy Story 1, 2 e 3", "Operação Big Hero"]
---
+++ Aprendendo Swift do Iniciante ao Avançado/Seção 4 | Arrays/23 | Método .Count.playground/Contents.swift
@@ -0,0 +1,30 @@
+// Criando uma array com conteúdo INT de forma IMPLÍCITA
+var datasDeCopaDoMundo = [1958, 1962, 1970, 1994, 2002]
+
+// Criando uma array com conteúdo INT de forma EXPLÍTITA
+var datasDeCopaDoMundo2 : [Int] = [1958, 1962, 1970, 1994, 2002]
+
+// Criando uma array com conteúdo STRING de forma IMPLÍCITA
+var filmesQueGosto = ["Tropa de Elite", "Toy Story", "Toy Story 2", "Toy Story 3", "Operação Big Hero"]
+
+// Criando uma array com conteúdo STRING de forma EXPLÍCITA
+var filmeQueGosto2 : [String] = ["Tropa de Elite", "Toy Story 1, 2 e 3", "Operação Big Hero"]
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+// Adicionando um item em uma array já criada
+datasDeCopaDoMundo.append(2020)
+datasDeCopaDoMundo2.append(2020)
+filmesQueGosto.append("O Artista")
+filmeQueGosto2.append("O Artista")
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+// Método .Count
+datasDeCopaDoMundo.count
+datasDeCopaDoMundo2.count
+filmesQueGosto.count
+filmeQueGosto2.count
+
+// Adicionando o resultado de um método .count de uma array com conteúdo STRING em uma array com conteúdo INT
+datasDeCopaDoMundo.append(filmesQueGosto.count)
