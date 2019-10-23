991707f20818cb6df1d82c9ded21715c9e486293
Otávio Anselmo <otavio.aanselmo@gmail.com>
2019-10-23 15:54:36 -0300

    session4_classes23to25

---
+++ Aprendendo Swift do Iniciante ao Avançado/Seção 4 | Arrays/25 | O que é o tal do Index?.playground/Contents.swift
@@ -0,0 +1,23 @@
+// Arrays
+
+var emojis = ["😀", "😇", "🙃", "🤗", "😎"]
+
+/* Retornando o primeiro elemento de uma array */
+emojis.first
+
+/* Retornando o último elemento de uma array */
+emojis.last
+emojis[emojis.count - 1]
+
+/* Retornando um elemento específico de uma array */
+emojis[0]
+emojis[1]
+emojis[2]
+emojis[3]
+emojis[4]
+
+/* Substituindo um emoji na array */
+emojis[1] = "😈"
+emojis[1] = "😼"
+emojis[emojis.count - 1] = "😴"
+emojis
