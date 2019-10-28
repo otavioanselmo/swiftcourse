efbe209637bf8759bfa82915b4bc935786b1df8e
Otávio Anselmo <otavio.aanselmo@gmail.com>
2019-10-28 09:51:26 -0300

    session5_class32

---
+++ Aprendendo Swift do Iniciante ao Avançado/Seção 5 | Booleans e Condicionais/32 | O começo de Condicionais.playground/Contents.swift
@@ -0,0 +1,13 @@
+// Booleans & Condicionais
+
+var nome : String = "Pedro"
+
+var maiorDeIdade : Bool = true
+
+// Se for FALSE, o compilador vai ignorar o que está dentro do IF. Se for TRUE, o compilador executa o que está dentro do IF.
+
+if maiorDeIdade {
+    print("\(nome) pode tirar a carteira de motorista.")
+}
+
+print("\(nome) é um cara legal")
