d2a0ad1c5b9e21564b5b6f78ecc6325e2db00cb2
Otávio Anselmo <otavio.aanselmo@gmail.com>
2019-11-01 15:11:36 -0300

    classesFromSession5

---
+++ Aprendendo Swift do Iniciante ao Avançado/Seção 5 | Booleans e Condicionais/25 | Exercício.playground/Contents.swift
@@ -0,0 +1,18 @@
+// Exercício
+
+// Crie uma condicional IF que contenha dentro de si outro código condicional IF/ELSE e que imprima dados no console
+
+var aniversarioDeAmigo : Bool = true
+var dinheiroNoMeuBolso : Double = 75.3
+
+if aniversarioDeAmigo {
+    print("Irei visitá-lo agora mesmo.")
+
+    if dinheiroNoMeuBolso >= 75 {
+        print("Além de visitá-lo, vou levar um presente bem bacana.")
+    } else {
+        print("Estou pobre, e um abraço vai ser o suficiente, afinal, ele é meu amigo.")
+    }
+}
+
+
---
+++ Aprendendo Swift do Iniciante ao Avançado/Seção 5 | Booleans e Condicionais/34 | Else.playground/Contents.swift
@@ -0,0 +1,13 @@
+// Booleans & Condicionais
+
+// Utilizando o Else (senão | outro)
+
+var nome = "Pedro"
+
+var idade = 16
+
+if idade >= 18 {
+    print("\(nome) é maior de idade.")
+} else {
+    print("\(nome) é menor de idade.")
+}
