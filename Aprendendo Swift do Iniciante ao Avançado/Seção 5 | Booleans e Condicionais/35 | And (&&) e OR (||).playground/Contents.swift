101ff72fa36888941c79db5d05fb72511987ec28
Otávio Anselmo <otavio.aanselmo@gmail.com>
2019-11-04 10:19:29 -0300

    session5_class35

---
+++ Aprendendo Swift do Iniciante ao Avançado/Seção 5 | Booleans e Condicionais/35 | And (&&) e OR (||).playground/Contents.swift
@@ -0,0 +1,18 @@
+// Operadores And (&&) e Or (||)
+
+var idade = 20
+
+idade >= 17
+idade <= 24
+
+// AND (&&)
+
+if idade >= 17 && idade <= 24 {
+    print("Esta informação está sendo impressa porque toda a condiconal é verdadeira neste caso.")
+}
+
+// OR (||)
+
+if idade >= 17 || idade <= 24 {
+    print("Esta informação está sendo impressa porque toda a condiconal é verdadeira neste caso.")
+}
