cd99a9f3404c3549523efc8ddba176af77174911
Otávio Anselmo <otavio.aanselmo@gmail.com>
2019-11-11 15:01:57 -0300

    att

---
+++ Aprendendo Swift do Iniciante ao Avançado/Seção 6 | Switch/41 | O que é Switch?.playground/Contents.swift
@@ -0,0 +1,14 @@
+// Switches
+
+var letra = "b"
+
+switch letra {
+case "a":
+    print("Esta é a primeira letra do alfabeto.")
+    
+case "z":
+    print("Esta é a última letra do alfabeto.")
+    
+default:
+    print("A letra escolhida não é nenhuma das duas anteriores.")
+}
